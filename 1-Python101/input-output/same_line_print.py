49091bf63e9e70dd1a9147224b644f08fd92fed9
sltm-14 <ie693242@iteso.mx>
2020-08-28 20:03:57 -0500

    ...

---
+++ 1-Python101/input-output/same_line_print.py
@@ -0,0 +1,10 @@
+print("This is the begining of the line, ", end ="")
+print("and this is the second part")
+
+print("This is the begining of the line, ", end ="--------")
+print("and this is the second part")
+
+n = 5
+
+for x in range(n):
+        print(str(x+1), end ="")
---
+++ 3-Problem_Set/leap_year.py
@@ -0,0 +1,15 @@
+def is_leap(year):
+    leap = False
+
+    if ( not(year % 400)):
+        leap = True
+    elif (not(year % 100)):
+        leap = False
+    elif (not(year % 4)):
+        leap = True
+
+    return leap
+
+year = int(input())
+
+print(is_leap(year))
