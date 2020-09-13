15ba1743e91ea79aec6445f6279d5a0bbec4593c
sltm-14 <ie693242@iteso.mx>
2020-09-12 23:58:16 -0500

    Calculadora

--- calc.py
+++ calc.py
@@ -1,2 +1,4 @@
-p = 4
-print(p)
+a = int(input("Ingresa una valor para A: "))
+b = int(input("Ingresa una valor para B: "))
+
+op = int(input('Qué operación desea hacer?\na) Suma\nb) Resta\nc) Multiplicación\nd) División'))
