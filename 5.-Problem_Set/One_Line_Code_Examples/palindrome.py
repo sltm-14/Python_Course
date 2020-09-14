f6f35358f1edd2a736c14e46d5541b3542d79996
sltm-14 <ie693242@iteso.mx>
2020-09-13 21:50:42 -0500

    ...

---
+++ 5.-Problem_Set/One_Line_Code_Examples/palindrome.py
@@ -0,0 +1,10 @@
+
+phrase = "anita lava la tina"
+
+print(phrase.find (phrase[::-1]))
+
+phrase = phrase.replace(" ", "")
+
+print(phrase)
+
+print(phrase.find (phrase[::-1]))
---
+++ 5.-Problem_Set/One_Line_Code_Examples/swap_two_variables.py
@@ -0,0 +1,7 @@
+
+a = 9
+b = 5
+
+a,b = b,a
+
+print("A: " + str(a) + " B: " + str(b))
