8020cd3b0a17940cb7d00c9dc8ff891ca0bafedf
sltm-14 <ie693242@iteso.mx>
2020-09-26 22:36:13 -0500

    graph example

---
+++ 5.-Problem_Set/printing_instances.py
@@ -0,0 +1,14 @@
+class Point:
+    """ Create a new Point, at coordinates x, y """
+
+    def __init__(self, x=0, y=0):
+        """ Create a new point at x, y """
+        self.x = x
+        self.y = y
+
+    def distance_from_origin(self):
+        """ Compute my distance from the origin """
+        return (self.x ) 
+
+p = Point(3, 4)
+print(p.distance_from_origin())
