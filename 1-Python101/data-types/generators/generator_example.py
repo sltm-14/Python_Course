226c42ad3f1c522268c44e0ff005255333794a25
sltm-14 <ie693242@iteso.mx>
2020-10-27 23:38:17 -0600

    generators

---
+++ 1-Python101/data-types/generators/generator_example.py
@@ -0,0 +1,8 @@
+import sys
+my_list = [i for i in range(10000)]
+print(sum(my_list))
+print(sys.getsizeof(my_list),"bytes\n")
+
+my_gen = (i for i in range(10000))
+print(sum(my_gen))
+print(sys.getsizeof(my_gen),"bytes")
