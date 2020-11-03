fd16beb163d0e2ddd25d37a208b626d10733277a
Carem <ie693242@iteso.mx>
2020-11-02 22:27:53 -0600

    best practices

---
+++ 1-Python101/good_practices/practices.py
@@ -0,0 +1,15 @@
+# 1.- Put most code into into a function or class
+# 2.- Use __name__ to control execution of code
+# 3.- Create a function called main() to contain the code you want to run
+# 4.- Call other functions from main()
+
+from time import sleep
+
+print("This is my file to demostrate best practices")
+
+def process_data(data):
+    print("Beginning data processing")
+    modified_data = data + " that has been modified"
+    sleep(3)
+    print("Data processing finished")
+    return modified_data
