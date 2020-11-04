2f0810f0a922042269995117eabf123e285476a6
sltm-14 <ie693242@iteso.mx>
2020-11-03 21:22:34 -0600

    good practices

--- 1-Python101/good_practices/practices.py
+++ 1-Python101/good_practices/practices.py
@@ -13,3 +13,12 @@ def process_data(data):
     sleep(3)
     print("Data processing finished")
     return modified_data
+
+def main():
+    data = "My data read from the web"
+    print(data)
+    modified_data = process_data (data)
+    print(modified_data)
+
+if __name__ == "__main__":
+    main()
